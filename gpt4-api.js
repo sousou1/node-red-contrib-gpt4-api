26379f30eb0d96892fd768a42878edd91552bdf5
So Tamura <so.tamura@sony.com>
2023-04-05 16:54:55 +0900

    Update for proxy

--- gpt4-api.js
+++ gpt4-api.js
@@ -1,20 +1,31 @@
 module.exports = function (RED) {
     const axios = require('axios');
 
+    // for proxy
+    const HttpProxyAgent = require('http-proxy-agent');
+    const HttpsProxyAgent = require('https-proxy-agent');
+
     function GPT4APINode(config) {
         RED.nodes.createNode(this, config);
+        const httpProxy = process.env.HTTP_PROXY || process.env.http_proxy;
+        const httpsProxy = process.env.HTTPS_PROXY || process.env.https_proxy;
         let node = this;
 
         node.on('input', function (msg) {
             const input_message = msg.payload;
             const system_message = config.system_message;
-            const api_key = node.context().global.get('gpt4_api_key'); // 変更: グローバル設定から api_key を取得
+            const api_key = node.context().global.get('gpt4_api_key'); 
             const model = config.model;
-            msg.topic = config.topic; // 追加：msg.topic を config から設定する
+            msg.topic = config.topic;
+
+            const proxyConfig = httpsProxy || httpProxy;
+            const agent = proxyConfig ? new HttpsProxyAgent(proxyConfig) : null;
 
             const instance = axios.create({
                 baseURL: 'https://api.openai.com/v1/chat/completions',
-                headers: { 'Authorization': 'Bearer ' + api_key }
+                headers: { 'Authorization': 'Bearer ' + api_key },
+                httpsAgent: agent, // 追加: プロキシ設定を追加
+                proxy: false // 追加: デフォルトのプロキシ設定を無効にする
             });
 
             const requestData = {
@@ -31,6 +42,8 @@ module.exports = function (RED) {
                     node.send(msg);
                 })
                 .catch(function (error) {
+                    node.warn(api_key);
+                    node.warn("error");
                     node.error(error);
                 });
         });
